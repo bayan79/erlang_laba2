0750529aa64cf7c98b31e402e7e0bbc583f2e0d0
bayan79 <bayandink@yandex.ru>
2020-11-17 01:39:48 +0500

    bugfix

--- src/database.erl
+++ src/database.erl
@@ -1,34 +1,25 @@
 -module(database).
 -include_lib("stdlib/include/qlc.hrl").
 
--export([init_db/1, info_db/1]).
+-export([init_db/1, info_db/1, get_db/1, store_db/2]).
 
 -record(person, {name, age}).
 
 init_db(Nodes) ->
     mnesia:create_schema(Nodes),
-    application:ensure_all_started(mnesia),
     [rpc:call(Node, mnesia, start, []) || Node <- Nodes],
-
-    mnesia:create_table(person, [
-        {attributes, record_info(fields, person)},
-        {type, set},
-        {frag_properties, [
-            {n_fragments, 6},
-%%            {n_disc_copies, 1},
-            {node_pool, Nodes}
-        ]}
-    ]),
-    Write = fun(Items) -> [mnesia:write(Item) || Item <- Items], ok end,
-    Persons = [
-%%        #person{name="Ivan", age=777},
-        #person{name="Elisa", age=555}
-    ],
-    mnesia:activity(sync_dirty, Write, [Persons], mnesia_frag),
-    Res = mnesia:activity(sync_dirty, fun(Item) -> mnesia:table_info(person, Item) end, [all], mnesia_frag),
-
-    io:fwrite("************* Res: ***************~n"),
-    [io:fwrite("{~p, ~p}~n", [Key, Value]) || {Key, Value} <- Res],
+    try
+        mnesia:create_table(person, [
+            {attributes, record_info(fields, person)},
+            {type, bag},
+            {frag_properties, [
+                {n_fragments, 6},
+                {node_pool, Nodes}
+            ]}
+        ])
+    after
+        body
+    end,
     ok.
 
 info_db(Info) ->
@@ -37,36 +28,19 @@ info_db(Info) ->
         end,
     Res = mnesia:activity(sync_dirty, F, [Info], mnesia_frag),
     Res.
-%%
-%%store_db(NodeName, {Size, Color}) ->
-%%    AF = fun() ->
-%%        mnesia:write(#ball{size=Size,
-%%                           color=Color})
-%%    end,
-%%    mnesia:transaction(AF).
-%%
-%%
-%%get_db(NodeName) ->
-%%    AF = fun() ->
-%%        Query = qlc:q([X || X <- mnesia:table(ball),
-%%            X#ball.node_name =:= NodeName]),
-%%        Results = qlc:e(Query),
-%%        lists:map(fun(Item) -> {Item#ball.size, Item#ball.color} end, Results)
-%%    end,
-%%    {atomic, Balls} = mnesia:transaction(AF),
-%%    Balls.
-%%
-%%
-%%delete_db(NodeName) ->
-%%    AF = fun() ->
-%%        Query = qlc:q([X || X <- mnesia:table(ball),
-%%            X#ball.node_name =:= NodeName]),
-%%        Results = qlc:e(Query),
-%%
-%%        F = fun() ->
-%%                lists:foreach(fun(Result) -> mnesia:delete_object(Result)
-%%            end, Results)
-%%        end,
-%%        mnesia:transaction(F)
-%%    end,
-%%    mnesia:transaction(AF).
+
+store_db(Name, Age) ->
+    AF = fun() -> mnesia:write(#person{name=Name, age=Age}) end,
+    mnesia:activity(sync_dirty, AF, [], mnesia_frag),
+    ok.
+
+unwrap_person(Person) ->
+    {Person#person.name, Person#person.age}.
+
+get_db(Name) ->
+    AF = fun() ->
+            Query = qlc:q([X || X <- mnesia:table(person), X#person.name =:= Name]),
+            Results = qlc:e(Query),
+            lists:map(fun unwrap_person/1, Results)
+         end,
+    mnesia:activity(sync_dirty, AF, [], mnesia_frag).
--- src/serv.erl
+++ src/serv.erl
@@ -18,29 +18,22 @@ start_link() ->
     gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-    io:format("hello~n"),
-    process_flag(trap_exit, true),
+    process_flag(trap_exit, false),
     NodesShortName = [host1@arch, host2@arch],
     [net_adm:ping(Node) || Node <- NodesShortName],
     database:init_db([node() | nodes()]),
     {ok, #state{}}.
 
 
-
-%%handle_call({store, {Size, red}}, _From, State) ->
-%%    database:store_db(1, {Size, "red"}),
-%%    {reply, ok};
-%%handle_call({store, {Size, _}}, _From, State) ->
-%%    database:store_db(2, {Size, "not red"}),
-%%    {reply, ok};
-%%handle_call({get, NodeName}, _From, State) ->
-%%    Results = database:get_db(NodeName),
-%%    {reply, Results};
+handle_call({store, {Name, Age}}, _From, _) ->
+    database:store_db(Name, Age),
+    {reply, ok};
+handle_call({get, Name}, _From, _) ->
+    Results = database:get_db(Name),
+    {reply, Results};
 handle_call({info, Info}, _From, _) ->
     Res = database:info_db(Info),
     {reply, Res}.
-
-
 
 
 handle_cast(_Msg, State) ->
